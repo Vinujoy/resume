8f7ddaed56f6ea9db53a9a55e312e2260eeb6d81
vinu joy <vinujoy2010@gmail.com>
2020-08-29 10:26:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './tailwind.output.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-5xl">hello</h1>
+    </div>
+  );
+}
+
+export default App;
